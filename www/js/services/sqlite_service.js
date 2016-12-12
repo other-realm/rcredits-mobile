d6206bf5d1dc6e78935ffc7c3f496c8be0d693f9
Aaron E-J <the@otherrealm.org>
2016-12-12 15:33:11 -0500

    Realized how much work it would be to switch from web sql to indexeddb - going to go back to trying a polyfill solution

--- www/js/services/sqlite_service.js
+++ www/js/services/sqlite_service.js
@@ -2,88 +2,58 @@
 (function (app) {
 	app.service('SQLiteService', function ($q, $timeout, NotificationService, localStorageService) {
 		var self;
-		var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
 		var SQLiteService = function () {
 			self = this;
 //			this.sqlPlugin = window.sqlitePlugin || window;
-			this.sqlPlugin = window;
-			this.db = indexedDB.open(window.rCreditsConfig.SQLiteDatabase.name, 1);
-		};
-		SQLiteService.prototype.open().onupgradeneeded = function () {
-			var ldb = this.db.result;
-			var store = ldb.createObjectStore('members', {
-				// record of customers (and managers)
-				keyPath: "qid"
-			});
-			var index = store.createIndex("NameIndex", ["name.last", "name.first"]);
+//			this.sqlPlugin = window;
+			this.db = null;
 		};
 //		SQLiteService.prototype.isDbEnable = function () {
 //			console.log(this.sqlPlugin);
 //			return !!this.sqlPlugin;
 //		};
 		SQLiteService.prototype.createDatabase = function () {
-			var openPromise = $q.defer();
-			if (window.cordova) {
-				this.db = $cordovaSQLite.openDB({name: "rcredits.db"}); //device
-			} else {
-				try {
-//					if (!window.openDatabase) {
-//						console.log('the database does not work!');
-//					} else {
-						console.log(true);
-						this.sqlPlugin.openDatabase(
-							window.rCreditsConfig.SQLiteDatabase.name,
-							window.rCreditsConfig.SQLiteDatabase.version,
-							window.rCreditsConfig.SQLiteDatabase.description, 100000);
-					this.db = 
-				"qid TEXT," + // customer or manager account code (like NEW.AAA or NEW:AAA)
-				"name TEXT," + // full name (of customer or manager)
-				"company TEXT," + // company name, if any (for customer or manager)
-				"place TEXT," + // customer location / manager's company account code
-				"balance REAL," + // current balance (as of lastTx) / manager's rCard security code
-				"rewards REAL," + // rewards to date (as of lastTx) / manager's permissions / photo ID (!rewards.matches(NUMERIC))
-				"lastTx INTEGER," + // unixtime of last reconciled transaction / -1 for managers
-				"proof TEXT," +
-				"photo TEXT);" // lo-res B&W photo of customer (normally under 4k) / full res photo for manager
-						$timeout(function () {
-							openPromise.resolve();
-						}, 1000);
-//					}
-				} catch (e) {
-					if (e === 2) {
-						console.log('wrong version - ', e);
-					} else {
-						console.log(e);
-					}
-				}
-			}
-			return openPromise.promise;
+			this.db = new Dexie(window.rCreditsConfig.SQLiteDatabase.name);
+			this.db.version(1).stores({
+				members:
+					"++," + //local indexeddb id
+					"qid," + // customer or manager account code (like NEW.AAA or NEW:AAA)
+					"name," + // full name (of customer or manager)
+					"company," + // company name, if any (for customer or manager)
+					"place," + // customer location / manager's company account code
+					"balance," + // current balance (as of lastTx) / manager's rCard security code
+					"rewards," + // rewards to date (as of lastTx) / manager's permissions / photo ID (!rewards.matches(NUMERIC))
+					"lastTx," + // unixtime of last reconciled transaction / -1 for managers
+					"proof," +
+					"photo", // lo-res B&W photo of customer (normally under 4k) / full res photo for manager"
+				txt:
+					"++," + //local indexeddb id
+					"me," + // company (or device-owner) account code (qid)
+					"txid 0," + // transaction id (xid) on the server (for offline backup only -- not used by the app) / temporary storage of customer cardCode pending tx upload
+					"status," + // see A.TX_... constants
+					"created," + // transaction creation datetime (unixtime)
+					"agent," + // qid for company and agent (if any) using the device
+					"member," + // customer account code (qid)
+					"amount," +
+					"goods," + // <transaction is for real goods and services>
+					"data," + // hash of cardCode, amount, created, and me (as proof of agreement)
+					"account," + //account particulars
+					"description" // always "reverses..", if this tx undoes a previous one (previous by date)
+			});
 		};
 		SQLiteService.prototype.ex = function () {
 			var txPromise = $q.defer();
 			txPromise.resolve(true);
 			return txPromise.promise;
 		};
-		SQLiteService.prototype.executeQuery_ = function (query, params) {
-			var txPromise = $q.defer();
-//			console.log(query, params);
-			this.db.transaction(function (tx) {
-				tx.executeSql(query, params, function (tx, res) {
-					txPromise.resolve(res);
-				}, function (tx, e) {
-					console.log(tx, e);
-//					var alertPopup = NotificationService.showAlert({
-//						title: "error",
-//						template: "There was an error: " + e.message
-//					});
-					txPromise.reject(e.message);
-				});
+		SQLiteService.prototype.executeQuery = function (query, params) {
+			this.db.transaction('rw', this.db.members, this.db.txt, function () {
+				
 			}, function (error) {
 				console.log('transaction error: ' + error.message);
 				console.log(error);
 			}, function () {
 			});
-			return txPromise.promise;
 		};
 		SQLiteService.prototype.executeQuery = function (sqlQuery) {
 			return this.executeQuery_(sqlQuery.getQueryString(), sqlQuery.getQueryData());
@@ -120,10 +90,10 @@
 			});
 		};
 		SQLiteService.prototype.init = function () {
-			if (!this.isDbEnable()) {
-				console.log("SQLite is not enable");
+			if (!this.db()) {
+				console.log("SQLite is not enabled");
 			}
-			this.createDatabase().then(this.createSchema.bind(this));
+			this.createDatabase();
 			console.log(this);
 		};
 		return new SQLiteService();
