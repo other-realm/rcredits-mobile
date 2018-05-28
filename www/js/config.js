82b3f3900deabd1a8f443f8108c09993d4583595
Aaron E-J <the@otherrealm.org>
2018-05-28 15:34:12 -0400

    Showing William git

--- www/js/config.js
+++ www/js/config.js
@@ -1,5 +1,8 @@
-// Most app config should go here. Use the BUILD_TARGET directives below to set config that depends on
-// whether the app is in dev mode, staging mode, etc.
+/**
+ * Most app config should go here. Use the BUILD_TARGET directives below to set config that depends on
+whether the app is in dev mode, staging mode, etc.
+ * @type type
+ */
 CommonGoodConfig = {
 	SQLiteDatabase: {
 		name: 'cg',
@@ -12,6 +15,6 @@ CommonGoodConfig = {
 	serverproxyUrl: 'https://ws.rcredits.org/pos',
 	serverUrl: 'https://ws.rcredits.org/pos',
 	version: '3.0',
-	build: 301,
+	build: 221,
 	transaction_max_amount_offline: 300
 };
